f18a30f3827ac23c7461e297e7a0ea562efc0348
NoXPhasma <noxphasma@live.de>
2022-06-06 22:07:46 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # zsh-systemctl
 A systemctl plugin for zsh.
+
+This plugin adds aliases for systemctl commands with sudo if needed.
+
+## Usage
+
+Some examples:
+
+```bash
+# Before
+systemctl status nginx
+# After:
+sc-status nginx
+
+# Before
+sudo systemctl restart nginx
+# After
+sc-restart nginx
+
+# It also supports --user services
+# Before
+systemctl --user status pipewire
+# After
+scu-status pipewire
+```
---
+++ init.zsh
@@ -0,0 +1,29 @@
+user_commands=(
+  list-units is-active status show help list-unit-files
+  is-enabled list-jobs show-environment cat list-timers)
+
+sudo_commands=(
+  daemon-reload start stop reload restart try-restart isolate kill
+  reset-failed enable disable reenable preset mask unmask
+  link load cancel set-environment unset-environment
+  edit)
+
+local_user_commands=(
+  list-units is-active status show help list-unit-files
+  is-enabled list-jobs show-environment cat list-timers
+  daemon-reload start stop reload restart try-restart isolate kill
+  reset-failed enable disable reenable preset mask unmask
+  link load cancel set-environment unset-environment edit)
+
+for c in $user_commands; do; alias sc-$c="systemctl $c"; done
+for c in $sudo_commands; do; alias sc-$c="sudo systemctl $c"; done
+
+alias sc-enable-now="sc-enable --now"
+alias sc-disable-now="sc-disable --now"
+alias sc-mask-now="sc-mask --now"
+
+for c in $local_user_commands; do; alias scu-$c="systemctl --user $c"; done
+
+alias scu-enable-now="scu-enable --now"
+alias scu-disable-now="scu-disable --now"
+alias scu-mask-now="scu-mask --now"
